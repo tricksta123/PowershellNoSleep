f40356353c2034346ec7298fdf57635667aab80c
tricksta123 <trevor@virtulus.co.uk>
2017-09-06 08:30:04 +0100

    Create PowershellNoSleep.ps1

---
+++ PowershellNoSleep.ps1
@@ -0,0 +1,11 @@
+#Enter the amounf of time in minutes you would like the script to run for.
+param($minutes = 60)
+
+#Invoke Windows Script Host
+$myshell = New-Object -com "Wscript.Shell"
+
+#Loop and press the shift key every minute until the specificed number of minutes is reached
+for ($i = 0; $i -lt $minutes; $i++) {
+  Start-Sleep -Seconds 1
+  $myshell.sendkeys("+")
+}
