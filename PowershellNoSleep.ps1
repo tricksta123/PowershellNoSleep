16160f3ccaeb17dc190dd83ec0f3d23d40291b1d
tricksta123 <trevor@virtulus.co.uk>
2018-01-03 11:08:43 +0000

    Update PowershellNoSleep.ps1

--- PowershellNoSleep.ps1
+++ PowershellNoSleep.ps1
@@ -1,11 +1,27 @@
-#Enter the amounf of time in minutes you would like the script to run for.
-param($minutes = 540)
+$scroll = "/-\|/-\|"
+$idx = 0
+$origpos = $host.UI.RawUI.CursorPosition
+$origpos.Y += 1
 
+#Enter the amount of time in minutes you would like the script to run for.
+$minutes = 540
 #Invoke Windows Script Host
 $myshell = New-Object -com "Wscript.Shell"
 
+write-host "Preventing Sleep" -NoNewline
+
 #Loop and press the shift key every minute until the specificed number of minutes is reached
+
 for ($i = 0; $i -lt $minutes; $i++) {
-  Start-Sleep -Seconds 1
+  $host.UI.RawUI.CursorPosition = $origpos 
+  Write-Host $scroll[$idx] -NoNewline 
+  $idx++ 
+  if ($idx -ge $scroll.Length) 
+  {  
+    $idx = 0 
+  }  
+  Start-Sleep -Seconds 1  
   $myshell.sendkeys("+")
 }
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Host ' '
